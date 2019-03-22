6336c0c62a9c7104eb63c72aa7b498602dfd1185
Paul Linck <paul.linck@gmail.com>
2019-03-22 19:35:09 -0400

    Blows Up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase'; // Firebase instance and context
+// Top ensure firebase is only instantiated once
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Doing it this way, we can be assured that Firebase is only instantiated once 
+// and that it is injected via React’s Context API to React’s component tree. 
+// Now, every component that is interested in using Firebase has access to the Firebase instance
+// with a FirebaseContext.Consumer component. 
+// Even though you will see it first-hand later for this application, the following code snippet shows how it would work:
+// import React from 'react';
+// import  { FirebaseContext } from '../Firebase';
+// const SomeComponent = () => (
+//   <FirebaseContext.Consumer>
+//     {firebase => {
+//       return <div>I've access to Firebase and render something.</div>;
+//     }}
+//   </FirebaseContext.Consumer>
+// );
+// export default SomeComponent;
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
+    document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
