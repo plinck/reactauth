c46641841aba364f9f00b3733f674605ab820f20
Paul Linck <paul.linck@gmail.com>
2019-03-23 10:40:08 -0400

    Split out form

---
+++ src/components/SignUp/SignUpForm.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    // use spread to deconstruct and save initial state
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
+
+    const isInvalid =
+    passwordOne !== passwordTwo ||
+    passwordOne === '' ||
+    email === '' ||
+    username === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <div class="row">
+            <div class="col s3 offset-s8">
+                <button disabled={isInvalid} class="btn btn-large waves-effect waves-light blue" type="submit" name="action">Submit
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </div>
+        
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+export default SignUpForm;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SignUpForm from "./SignUpForm";
 
 import * as ROUTES from '../../constants/routes';
 
-const SignUpPage = () => (
-  <div>
-    <h1>SignUp</h1>
-    <SignUpForm />
-  </div>
-);
+class SignUpPage extends React.Component {
+  constructor(props) {
+      super(props);
+  }
+
+  render() {
+      return ( 
+        <div>
+          <h1>SignUp</h1>
+          <SignUpForm />
+        </div>
+      );
+  }
+}
 
 const INITIAL_STATE = {
   username: '',
@@ -17,82 +26,21 @@ const INITIAL_STATE = {
   passwordTwo: '',
   error: null,
 };
-
-class SignUpForm extends Component {
+class SignUpLink extends React.Component {
   constructor(props) {
-    super(props);
-
-    // use spread to deconstruct and save initial state
-    this.state = { ...INITIAL_STATE };
+      super(props);
   }
-
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
 
   render() {
-    const {
-      username,
-      email,
-      passwordOne,
-      passwordTwo,
-      error,
-    } = this.state;
-
-    const isInvalid =
-    passwordOne !== passwordTwo ||
-    passwordOne === '' ||
-    email === '' ||
-    username === '';
-
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <div class="row">
-            <div class="col s3 offset-s8">
-                <button disabled={isInvalid} class="btn btn-large waves-effect waves-light blue" type="submit" name="action">Submit
-                    <i class="material-icons right">send</i>
-                </button>
-            </div>
+      return ( 
+        <div>
+          <p>
+            Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+          </p>
         </div>
-        
-        {error && <p>{error.message}</p>}
-      </form>
-    );
+      );
   }
 }
-const SignUpLink = () => (
-  <p>
-    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-  </p>
-);
 
 export default SignUpPage;
 
