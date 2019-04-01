eecd61c30d6720fcc2129b4d79f2d10efde2fa60
plinck <paul.linck@gmail.com>
2019-04-01 19:03:47 -0400

    Hardcoded env for now since .env isnt working in REACT with Node

--- client/src/components/Auth/Firebase/firebase.js
+++ client/src/components/Auth/Firebase/firebase.js
@@ -2,16 +2,27 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 require("dotenv").config();
 
+// For the life of me I cant get REACT (when in client/) to read ENV vars and google isnt helping me ...
 // Firebase Config using react env
+// const config = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+// };
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyCGvVrq9uuUgTF7nBXzPIqUzml42YUtN08",
+  authDomain: "paullinck-authreact.firebaseapp.com",
+  databaseURL: "https://paullinck-authreact.firebaseio.com",
+  projectId: "paullinck-authreact",
+  storageBucket: "paullinck-authreact.appspot.com",
+  messagingSenderId: "910164644412"
 };
 
+console.log("fb config", config);
+console.log("process.env", process.env);
 class Firebase {
   constructor() {
     try {
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import M from "materialize-css/dist/js/materialize.min.js";
 import SignOutButton from '../Auth/SignOut/SignOut';
 import AuthUserContext from '../Auth/Session/AuthUserContext';
 
@@ -16,32 +17,44 @@ class Navigation extends React.Component {
       }
   }
 
+  componentDidMount() {
+    let elem = document.querySelector(".sidenav");
+    M.Sidenav.init(elem, {
+        edge: "left",
+        inDuration: 250
+    });
+  }
+
   render() {
     const navigationAuth = 
       <ul>
-        <li>
-          <Link to="/">Landing</Link>
-        </li>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/account">Account</Link>
-        </li>
-        <li>
-          <SignOutButton />
-        </li>
+        <li><Link to="/">Landing</Link></li>
+        <li><NavLink to="/home">Home</NavLink></li>
+        <li><NavLink to="/account">Account</NavLink></li>
+        <li><SignOutButton /></li>
       </ul>
       ;
 
     const navigationNonAuth = 
       <ul>
-        <li>
-            <Link to="/">Landing</Link>
-        </li>
-        <li>
-            <Link to="/signin">Signin</Link>
-        </li>
+        <li><Link to="/">Landing</Link></li>
+        <li><NavLink to="/signin">Signin</NavLink></li>
+      </ul>
+    ;
+
+    const mobileNavigationAuth = 
+      <ul>
+        <li><a href="/">Landing</a></li>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/account">Account</a></li>
+        <li><SignOutButton /></li>
+      </ul>
+      ;
+
+    const mobileNavigationNonAuth = 
+      <ul>
+        <li><a href="/">Landing</a></li>
+        <li><a href="/signin">Signin</a></li>
       </ul>
     ;
 
@@ -49,20 +62,15 @@ class Navigation extends React.Component {
     // Not the AuthUSerContext Provider passes the authUser
     // in its value={} paramater (see withAuthentication component in Auth/Session)
     // ANY COMPONENT that needs authUser info uses consumer this way
-    const Navigation = () => (
-      <div>
-        <AuthUserContext.Consumer>
-          {authUser =>
-            authUser ? navigationAuth : navigationNonAuth
-          }
-        </AuthUserContext.Consumer>
-      </div>
-    );
       
     return (
       <div>
         <ul className="sidenav" id="mobile-menu">
-            <Navigation />
+          <AuthUserContext.Consumer>
+            {authUser =>
+              authUser ? mobileNavigationAuth : mobileNavigationNonAuth
+            }
+          </AuthUserContext.Consumer>
         </ul>
 
         <div className='navbar-fixed'>
@@ -70,7 +78,11 @@ class Navigation extends React.Component {
                 <div className="container nav-wrapper">
                     <a href="#!" data-target="mobile-menu" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                     <ul className="right hide-on-med-and-down">
-                        <Navigation />
+                      <AuthUserContext.Consumer>
+                        {authUser =>
+                          authUser ? navigationAuth : navigationNonAuth
+                        }
+                      </AuthUserContext.Consumer>
                     </ul>
                 </div>
             </nav>
